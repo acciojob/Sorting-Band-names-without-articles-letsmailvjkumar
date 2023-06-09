58eaae2471a72f3ebda59ee13753bdbff94567f8
VJKUMAR <115359380+letsmailvjkumar@users.noreply.github.com>
2023-06-09 20:57:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
-touristSpots.sort()
-console.log(touristSpots)
+const bandNames = ['The Beatles', 'Aerosmith', 'The Rolling Stones', 'Led Zeppelin', 'Nirvana', 'The Who'];
+
+function removeArticles(name) {
+  const articles = ['the', 'a', 'an'];
+  const words = name.split(' ');
+  const filteredWords = words.filter(word => !articles.includes(word.toLowerCase()));
+  return filteredWords.join(' ');
+}
+
+const sortedBandNames = bandNames.sort((a, b) => removeArticles(a) > removeArticles(b) ? 1 : -1);
+
+const bandList = document.querySelector('#band');
+sortedBandNames.forEach(bandName => {
+  const listItem = document.createElement('li');
+  listItem.textContent = bandName;
+  bandList.appendChild(listItem);
+});
